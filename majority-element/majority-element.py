d714f6d815c8fa9d4b9bbacdd725eae647341407
MohammedBasheerUddin <66587848+MohammedBasheerUddin@users.noreply.github.com>
2021-07-30 11:47:56 +0530

    Time: 156 ms (94.43%), Space: 15.5 MB (53.12%) - LeetHub

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,10 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        s=list(set(nums))        # keep unique values in s
-        l=len(nums)/2            # store length of nums // 2 ,to check majority within array
-        for i in s:              # go through only unique one . i.e the repeated ones or duplicate ones .
-            if nums.count(i)>l:  # check if thier count is greated than majority criteria
-                return i         # return the winner
+        s=list(set(nums))        # keep unique values in s.
+        l=len(nums)/2            # store length of nums // 2 ,to check majority within array.
+        for i in s:              # go through only unique one . i.e the repeated ones or duplicate ones who stand a chance to win by majority.
+            if nums.count(i)>l:  # check if thier count is greated than majority criteria.
+                return i         # return the winner.
             
 
         
